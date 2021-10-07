43acc1260f47c4a8de28966b542aca9d5afb017e
juan-calle <juancmcalle@gmail.com>
2021-10-07 02:58:11 +0200

    :rotating_light: Add husky and lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
-// module.exports = {
-//   './**/*.{js,ts}': files => [
-//     `eslint ${files.join(' ')} --cache --fix --no-error-on-unmatched-pattern`,
-//     `prettier ${files.join(' ')} --write --no-error-on-unmatched-pattern`,
-//   ],
-// };
+module.exports = {
+  './**/*.{js,ts}': files => [
+    `eslint ${files.join(' ')} --cache --fix --no-error-on-unmatched-pattern`,
+    `prettier ${files.join(' ')} --write --no-error-on-unmatched-pattern`,
+  ],
+};
